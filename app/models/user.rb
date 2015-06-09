cf10c31c23403a30398f70502fda8f1c5959862f
Joshua C. Eubanks <ubmeander@gmail.com>
2015-06-09 12:38:40 -0400

    initial

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ActiveRecord::Base
+	has_many :blogs
+	has_many :comments
+
+	validates :name, presence: true
+	validates :email, presence: true
+	
+	extend FriendlyId
+	friendly_id :name, use: :slugged
+end
